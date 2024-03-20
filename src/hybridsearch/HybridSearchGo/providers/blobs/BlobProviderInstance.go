49d9c14a0d91d79170a9d971cd41125ad6781377
Matthew Whited <mwhited@eliassen.com>
2024-03-20 17:03:12 -0400

    stub out fixes

--- src/hybridsearch/HybridSearchGo/providers/blobs/BlobProviderInstance.go
+++ src/hybridsearch/HybridSearchGo/providers/blobs/BlobProviderInstance.go
@@ -1,6 +1,7 @@
 package blobs
 
 import (
+	"context"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 	"hybrid-search/webapi/models"
 )
@@ -14,7 +15,25 @@ func (provider BlobProviderInstance) GetContent(file string) models.ContentRefer
 	return models.ContentReference{}
 }
 func (provider BlobProviderInstance) List() []models.SearchResultModel {
-	return []models.SearchResultModel{}
+
+	var result []models.SearchResultModel
+
+	pager := provider.Client.NewListBlobsFlatPager(provider.CollectionName, nil)
+	for pager.More() {
+		resp, _ := pager.NextPage(context.TODO()) //TODO: do something with the error
+		for _, item := range resp.Segment.BlobItems {
+			model := models.SearchResultModel{
+				Content:  "", //TODO: do something else here
+				File:     *item.Metadata["File"],
+				PathHash: *item.Metadata["PathHash"],
+				Score:    1,
+				Type:     models.SearchTypeNone,
+			}
+			result = append(result, model)
+		}
+	}
+
+	return result
 }
 func (provider BlobProviderInstance) TryStore(full string, file string, pathHash string) bool {
 	return false
